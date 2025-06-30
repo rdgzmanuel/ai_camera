12082770a68b6ead2c3c7f4292773b4dbb5d7192
rdgzmanuel <manuel.rodriguezvillegas09@gmail.com>
2025-06-30 11:02:46 +0200

    YOLOv3 finetuning.

--- src/train_yolo.py
+++ src/train_yolo.py
@@ -1,17 +1,16 @@
 from ultralytics import YOLO
 
 def main():
-    model = YOLO("models/yolo11m.pt")  # or yolov11m.pt, etc.
+    model = YOLO("models/yolo11m.pt")
 
     model.train(
         data="football.yaml",
         epochs=16,
         imgsz=640,
         batch=16,
-        lr0=1e-3,
+        lr0=1e-4,
         optimizer="Adam",
-        freeze=[0],
-        name="yolov11_2"
+        name="yolov11_3"
     )
 
 if __name__ == "__main__":
